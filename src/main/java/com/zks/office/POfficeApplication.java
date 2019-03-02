102f6db94e6d5218a3cd79b964c9151e710c6d2a
zhaokunshan <3162356820@qq.com>
2019-03-02 18:21:51 +0800

    COMMIT P-OFFICE

---
+++ src/main/java/com/zks/office/POfficeApplication.java
@@ -0,0 +1,13 @@
+package com.zks.office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class POfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(POfficeApplication.class, args);
+    }
+
+}
